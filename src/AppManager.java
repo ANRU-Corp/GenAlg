7e1075ff69bb165726828b335d911664a87e34e3
ANRU-Corp <antoni@rucinski.pol.pl>
2017-11-05 16:42:02 +0100

    first commit

---
+++ src/AppManager.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AppManager {
+    public static void main(String[] args) throws IOException {
+
+
+        List<Integer> list = new ArrayList<Integer>();
+        list = ModelManager.getBoardFromFile("/home/anru/Documents/PW/GenAl/src/sudoku.txt");
+        System.out.print(list.get(15));
+
+
+    }
+
+
+
+
+
+}
